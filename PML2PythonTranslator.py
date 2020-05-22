e73447355d0378c1e2b969ae7739e0a720c37f55
polarkernel <polarkernel@polarkernel.de>
2020-05-22 16:15:54 +0200

    error management added

--- PML2PythonTranslator.py
+++ PML2PythonTranslator.py
@@ -3,20 +3,33 @@ from antlr4 import FileStream, CommonTokenStream, ParseTreeWalker
 from pml_grammar.pmlLexer import pmlLexer
 from pml_grammar.pmlParser import pmlParser
 from PythonListener import PythonListener
-#from testListener import testListener
+from ExceptionManagement import ParserExceptionListener
 
 def main(argv):
     input_stream = FileStream(argv[1])
     lexer = pmlLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = pmlParser(stream)
-    tree = parser.styles()
-#    translator = testListener()
-    translator = PythonListener()
-    walker = ParseTreeWalker()
-    walker.walk(translator, tree)
 
-    sys.stdout.write( translator.getCode() )
+    parser.removeErrorListeners()
+    exceptionListener = ParserExceptionListener()
+    parser.addErrorListener( exceptionListener )
+
+    # error management
+    hadSyntaxErrors = False
+    try:
+        tree = parser.styles()
+    except Exception as e:
+        errorText = str(e)
+        hadSyntaxErrors = True
+
+    if not hadSyntaxErrors:  
+        translator = PythonListener()
+        walker = ParseTreeWalker()
+        walker.walk(translator, tree)
+        sys.stdout.write( translator.getCode() )
+    else:
+        sys.stdout.write(errorText)
 
 if __name__ == '__main__':
     main(sys.argv)
